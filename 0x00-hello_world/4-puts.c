7ddb3655679aaf216368a9e8f5c7dac3bf3facfa
nhamidn <hamid-savana@hotmail.fr>
2023-02-17 01:48:33 +0100

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main()
+/**
+*	main - Print a string using puts
+*/
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
