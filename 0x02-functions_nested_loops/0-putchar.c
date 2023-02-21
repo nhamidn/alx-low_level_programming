adbdc1db15fb062c8705fcfe6177f328680ac154
nhamidn <hamid-savana@hotmail.fr>
2023-02-21 15:52:07 +0100

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - prints _putchar followed by a new line
+* Return: 0 in case of success
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
