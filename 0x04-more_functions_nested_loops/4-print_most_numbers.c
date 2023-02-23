a459e7274a755f38cf6a6ffda5cc32b8105c03ea
nhamidn <hamid-savana@hotmail.fr>
2023-02-23 23:51:26 +0100

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ * from 1 to 9 except 2 and 4
+ *
+ * Return: Nothing (void function).
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		i++;
+	}
+}
