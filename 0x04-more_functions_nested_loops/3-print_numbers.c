a7975a3bf1f2f1e6f7829cf089a6d2b83231fbca
nhamidn <hamid-savana@hotmail.fr>
2023-02-23 23:44:21 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchat('\n');
+	return (0);
+}
