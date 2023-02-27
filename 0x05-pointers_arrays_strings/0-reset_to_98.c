6bff8bb99325657663a9c0eeb679d235e1245386
nhamidn <hamid-savana@hotmail.fr>
2023-02-27 21:05:42 +0100

    first test

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of a pointer to 98
+ * @n: the pointer to reset it value
+ * Return: Nothing (void function).
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
