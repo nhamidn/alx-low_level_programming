05f28a860d6e8b6b7b6189da28c9b03cca07b9d4
nhamidn <hamid-savana@hotmail.fr>
2023-03-03 00:12:50 +0100

    typo

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
+ * _strncat - concatenates two strings
  * @dest: destination string
  * @src: source string
  * @n: number of chars to copy to dest
