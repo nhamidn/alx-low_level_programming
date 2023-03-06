23c2d2b03e7918917b342309f2867e0fc430a3fa
nhamidn <hamid-savana@hotmail.fr>
2023-03-07 00:58:02 +0100

    2-strchr.c fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,9 +9,9 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*str != c && *str)
-		str++;
-	if (*str == c)
-		return (str);
+	while (*s != c && *s)
+		s++;
+	if (*s == c)
+		return (s);
 	return (NULL);
 }
