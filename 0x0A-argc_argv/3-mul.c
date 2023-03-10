7bbff9bd9dab36323c51618a460f67ce087fec36
nhamidn <hamid-savana@hotmail.fr>
2023-03-10 13:32:13 +0100

    typo

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,16 @@
  */
 int main(int argc, char **argv)
 {
+	int r;
+
 	if (argc == 3)
 	{
-		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		r = atoi(argv[1]) * atoi(argv[2])
+		printf("%d\n", r);
 	}
 	else
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		return (1);
 	}
 	return (0);
