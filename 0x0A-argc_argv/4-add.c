e9e6cd0cd0dc46f7aa38205d47ad805a813212f3
nhamidn <hamid-savana@hotmail.fr>
2023-03-10 13:07:39 +0100

    another try

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,7 +4,7 @@
  * _checker - check if a string
  * contain a non digit character
  * @str: string to check
- * Return: 1 if it's digit itherwise 0
+ * Return: 1 if it's digit otherwise 0
  */
 int _checker(char *str)
 {
@@ -25,23 +25,25 @@ int _checker(char *str)
  * @argv: array of arguments
  * Return: 0 on success 1 on error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	unsigned int sum = 0;
 	int i = 1;
 
-	if (argc > 1)
+	if (argc == 1)
 	{
-		while (i < argc)
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		if (_checker(argv[i]) == 0)
 		{
-			if (_checker(argv[i]) == 0)
-			{
-				printf("ERROR\n");
-				return (1);
-			}
-			sum = sum + atoi(argv[i]);
-			i++;
+			printf("ERROR\n");
+			return (1);
 		}
+		sum = sum + atoi(argv[i]);
+		i++;
 	}
 	printf("%d\n", sum);
 	return (0);
