cf547d80c390cd74d04911a9171dcf0548e5375c
nhamidn <hamid-savana@hotmail.fr>
2023-03-10 13:27:14 +0100

    correct version

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,29 +6,31 @@
  * @argv: array of arguments
  * Return: 0 on success 1 on error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int r = 0;
 	int i = 1;
 
-	if (argc > 1)
+	if (argc == 1)
 	{
-		while (i < argc)
-		{
-			int j = 0;
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		int j = 0;
 
-			while (argv[i][j])
+		while (argv[i][j])
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
 			{
-				if (argv[i][j] < 48 || argv[i][j] > 57)
-				{
-					printf("ERROR\n");
-					return (1);
-				}
-				j++;
+				printf("Error\n");
+				return (1);
 			}
-			r = r + atoi(argv[i]);
-			i++;
+			j++;
 		}
+		r = r + atoi(argv[i]);
+		i++;
 	}
 	printf("%d\n", r);
 	return (0);
