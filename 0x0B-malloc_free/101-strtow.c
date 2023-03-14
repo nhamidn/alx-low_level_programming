0fb270c4092170345d320e622910cedd0639775d
nhamidn <hamid-savana@hotmail.fr>
2023-03-14 09:23:07 +0100

    another implementation

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * strtow - split string into words
+ * strtowi - split string into words
  * @str: string to split
  * Return: 2d char array
  */
-char **strtow(char *str)
+char **strtowi(char *str)
 {
 	char **dest;
 	int i = -1, j = 0, len1 = 0, len2;
@@ -18,10 +18,10 @@ char **strtow(char *str)
 				len1++;
 	if (i == 1 || str == NULL || len1 == 0)
 		return (NULL);
-	dest = (char **)malloc(sizeof(char *) * (len1));
+	dest = (char **)malloc(sizeof(char *) * (len1 + 1));
 	if (dest == NULL)
 		return (NULL);
-	dest[len1 - 1] = NULL;
+	dest[len1] = NULL;
 	i = j = len1 = 0;
 	while (str[i] != '\0')
 	{
@@ -47,4 +47,75 @@ char **strtow(char *str)
 		j = i;
 	}
 	return (dest);
+}
+/**
+ * w - count words in a string
+ * @str: string
+ * Return: integer
+ */
+int w(char *str)
+{
+	int i = 0, len1 = 0;
+
+	while (str[++i] != '\0')
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+			if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n')
+				len1++;
+
+	return (len1);
+}
+/**
+ * charcount - count characters in a word
+ * @str: string
+ * @i: the starting index
+ * Return: integer
+ */
+int charcount(char const *str, int i)
+{
+	int count;
+
+	count = 0;
+	while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+	{
+		if (str[i] == '\0')
+			break;
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * strtow - split string into words
+ * @str: string to split
+ * Return: 2d char array
+ */
+char **strtow(char *str)
+{
+	char **st;
+	int i = 0, j = 0, wlen = 0, k;
+
+	wlen = w(str);
+	if (str == NULL || wlen == 0)
+		return (NULL);
+	st = (char **)malloc(sizeof(char *) * (wlen + 1));
+	if (st == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+		{
+			k = 0;
+			st[j] = malloc(sizeof(char) * charcount(str, i));
+			if (st[j] == NULL)
+				return (NULL);
+			while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\0')
+				st[j][k++] = str[i++];
+			st[j++][k] = '\0';
+		}
+		else
+			i++;
+	}
+	st[j] = NULL;
+	return (st);
 }
