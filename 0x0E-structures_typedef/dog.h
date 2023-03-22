3a30e11708395fe8f1d5e6a662a7b6919ee0a096
nhamidn <hamid-savana@hotmail.fr>
2023-03-22 01:15:01 +0000

    initial

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - Dog's struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
