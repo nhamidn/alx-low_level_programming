0e6c044246daed42ea53c110fa8a9d91aeac5339
nhamidn <hamid-savana@hotmail.fr>
2023-03-30 21:55:01 +0000

    add missing prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,8 +5,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-int _putchar(char c);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -21,5 +19,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
