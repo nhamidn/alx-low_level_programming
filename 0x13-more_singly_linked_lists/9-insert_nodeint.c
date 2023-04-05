bc6b651a0cdfabfaf54e726be4ced2bf61e156fd
nhamidn <hamid-savana@hotmail.fr>
2023-04-05 02:38:01 +0000

    another try

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node, *next, *tmp;
 
 	tmp = *head;
-	if (!head)
+	if (!head || !*head)
 		return (NULL);
 
 	node = malloc(sizeof(listint_t));
@@ -39,5 +39,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 		tmp = tmp->next;
 	}
-	return (node);
+	free(node);
+	return (NULL);
 }
