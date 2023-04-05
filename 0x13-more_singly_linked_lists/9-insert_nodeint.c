3cd3ddbeac0f6682cce19a4a2679dc8e3cc8365d
nhamidn <hamid-savana@hotmail.fr>
2023-04-05 02:45:22 +0000

    another try2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,6 +36,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			node->next = next;
 			return (node);
 		}
+		else if (tmp->next == NULL && (i + 1) == idx)
+		{
+			node->next = NULL;
+			tmp->next = node;
+			return (node);
+		}
 		i++;
 		tmp = tmp->next;
 	}
