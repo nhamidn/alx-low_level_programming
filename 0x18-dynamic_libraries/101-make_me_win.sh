f8514d0589b8697d7e1e62623ae6a179973c8fb0
Noureddine Hamid <noureddine_hamid@outlook.fr>
2023-05-26 17:39:07 -0700

    update branch name

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/hack_gm.so https://github.com/nhamidn/alx-low_level_programming/raw/master/0x18-dynamic_libraries/hack_gm.so
+wget -O /tmp/hack_gm.so https://github.com/nhamidn/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hack_gm.so
 export LD_PRELOAD=/tmp/hack_gm.so
