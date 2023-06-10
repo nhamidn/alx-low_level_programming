1331d2b144e84880fe6a1cae7c2932fceb3e1ba6
nhamidn <hamid-savana@hotmail.fr>
2023-06-10 01:28:20 +0100

    fix error

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,7 +5,7 @@
  * @str: string
  * Return: hash value
  */
-unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
